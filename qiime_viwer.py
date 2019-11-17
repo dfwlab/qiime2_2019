0088416110778bf7b969c9e0a06ef8f49fd91fdb
ddhmed <799390224@qq.com>
2019-11-17 08:41:03 +0800

    Update qiime_viwer.py

--- qiime_viwer.py
+++ qiime_viwer.py
@@ -12,7 +12,7 @@ repo = g.get_repo("gz16s/qiime_viwer")
 file_path = sys.argv[1]
 file_type = file_path.split('.')[-1]
 newfile = str(int(random.random()*10**10))+"."+file_type
-contents = open('/data/'+file_path, 'rb').read()
+contents = open(file_path, 'rb').read()
 repo.create_file(newfile, "upload", contents, branch="master")
 view_data = "<a href='https://view.qiime2.org/visualization/?type=html&src=https%3A%2F%2Fraw.githubusercontent.com%2Fgz16s%2Fqiime_viwer%2Fmaster%2F"+newfile+"' target='_blank'>在新窗口查看结果</a>"
 view_data += "<iframe width='100%', height='300px', src='https://view.qiime2.org/visualization/?type=html&src=https%3A%2F%2Fraw.githubusercontent.com%2Fgz16s%2Fqiime_viwer%2Fmaster%2F"+newfile+"' />"
